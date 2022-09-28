35f9789e4fe0871a940ede33afb389237f980742
EricLee543 <408228228@qq.com>
2022-09-28 11:15:51 +0800

    create a new record file when the a file too large

--- funcx_endpoint/funcx_endpoint/executors/high_throughput/execution_recorder.py
+++ funcx_endpoint/funcx_endpoint/executors/high_throughput/execution_recorder.py
@@ -7,8 +7,10 @@ class ExecutionRecorder:
     def __init__(self):
         self._FUNCX_HOME = os.path.join(pathlib.Path.home(), ".funcx")
         self._FUNCX_EXECUTION_RECORD = os.path.join(self._FUNCX_HOME, "execution_record")
-        self.max_files = 10 ** 6
+        self.doing_record = True
+        self.max_files = 100
         self._init_execution_record_dir()
+        self.latest_record_file = self._get_record_file()
 
     def _init_execution_record_dir(self):
         """Initialize the execution record directory"""
@@ -36,17 +38,41 @@ class ExecutionRecorder:
         file_list = os.listdir(self._FUNCX_EXECUTION_RECORD)
         file_list.sort(reverse=True)
         return os.path.join(self._FUNCX_EXECUTION_RECORD, file_list[0])
-            
         
-    
+
+    def _create_new_record_file(self):
+        #TODO
+        """
+         Here should be a strategies like RLU, to update the record
+        """
+        old_file = os.path.basename(self.latest_record_file)
+        new_name = int(self.latest_record_file) + 1
+        if new_name > self.max_files:
+            self.doing_record = False
+            return
+        new_file_path = os.path.join(self._FUNCX_EXECUTION_RECORD, "%08d" % new_name)
+        with open(new_file_path, mode="w") as f:
+                pass
+        self.latest_record_file = new_file_path
+
+            
+         
     def write_record(self,task_id, result):
         """Write the execution record to the execution record directory"""
 
-        record_file = self._get_record_file()
+        if self.doing_record : 
+            # information be append to a record file, so the file should be opened in append mode
+            with open(self.latest_record_file, "a") as f:
+                result_copy = result.copy()
+                result_copy["task_id"] = task_id
+                f = csv.DictWriter(f, result_copy.keys())
+                f.writerow(result_copy)
+
+            # get the size of record file
+            file_size = os.stat(self.latest_record_file).st_size / (1024*1024)
+
+            # file_size > 10mb, create a new one
+            if file_size > 10:
+                self._create_new_record_file()
+
         
-        # information be append to a record file, so the file should be opened in append mode
-        with open(record_file, "a") as f:
-            result_copy = result.copy()
-            result_copy["task_id"] = task_id
-            f = csv.DictWriter(f, result_copy.keys())
-            f.writerow(result_copy)
