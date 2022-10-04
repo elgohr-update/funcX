978facd1852aa93e3fa50b3d0757d0734861f448
EricLee543 <408228228@qq.com>
2022-10-04 19:21:30 +0800

    add rsync command generate

---
+++ funcx_endpoint/funcx_endpoint/data_transfer/rsync.py
@@ -0,0 +1,7 @@
+class RsyncTransferClient:
+    def __init__(self, local_path=".", dst_ep=None,sync_level="checksum",**kwargs):
+        self.local_path = local_path
+        self.dst_ep = dst_ep
+
+
+    def transfer(self, )
